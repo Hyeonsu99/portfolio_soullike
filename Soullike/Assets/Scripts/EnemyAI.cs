899b49fa5ed75c7cd32d305ed033dce97211eba7
Hyeonsu99 <johyeonsu08@gmail.com>
2024-12-20 02:10:08 +0900

    트리 노드 구성 중

--- Soullike/Assets/Scripts/EnemyAI.cs
+++ Soullike/Assets/Scripts/EnemyAI.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    private float _detectionRange = 10;
+    private float _detectionRange = 25;
     private float _attackRange = 3;
 
     public Transform _target;
@@ -43,54 +43,94 @@ public class EnemyAI : MonoBehaviour
                     (
                         new List<INode>()
                         {
-                            new ActionNode(Action2),
-
+                            new ActionNode(CheckEnemyInDetectionRange),
                             new ParallelNode
                             (
                                 new List<INode>()
                                 {
-                                    new ActionNode(Action3),
-                                    new ActionNode(Action1)
+                                    new SequenceNode
+                                    (
+                                        new List<INode>()
+                                        {
+                                            new ActionNode(CheckEnemyInAttackRange),
+                                            new ActionNode(Attack)
+                                        }
+                                    ),
+
+                                    new ActionNode(Chase)
                                 }
                             )
                         }
-                    )
+                    ),
+
                 }
             );
     }
 
-    INode.NodeState Action1()
+    INode.NodeState CheckEnemyInDetectionRange()
     {
+        var targets = Physics.OverlapSphere(transform.position, _detectionRange, layerMask);
+
         Debug.Log(System.Reflection.MethodBase.GetCurrentMethod().Name);
 
-        return INode.NodeState.Success;
-    }
+        if (targets.Length > 0 && targets != null)
+        {
+            _target = targets[0].transform;
 
-    INode.NodeState Action2()
-    {
-        Debug.Log(System.Reflection.MethodBase.GetCurrentMethod().Name);
-
-        return INode.NodeState.Success;
-    }
-    INode.NodeState Action3()
-    {
-        Debug.Log(System.Reflection.MethodBase.GetCurrentMethod().Name);
+            return INode.NodeState.Success;
+        }
 
         return INode.NodeState.Failure;
     }
-    INode.NodeState Action4()
+
+    INode.NodeState CheckEnemyInAttackRange()
     {
+        var targets = Physics.OverlapSphere(transform.position, _attackRange, layerMask);
+
         Debug.Log(System.Reflection.MethodBase.GetCurrentMethod().Name);
 
-        return INode.NodeState.Running;
+        if (targets.Length > 0 && targets != null)
+        {
+            _target = targets[0].transform;
+
+            return INode.NodeState.Success;
+        }
+
+        return INode.NodeState.Failure;
     }
-    INode.NodeState Action5()
+
+    INode.NodeState Attack()
     {
         Debug.Log(System.Reflection.MethodBase.GetCurrentMethod().Name);
 
         return INode.NodeState.Success;
     }
 
+    INode.NodeState Chase()
+    {
+        Debug.Log("추적 중..");
+
+        if (Vector3.Distance(transform.position, _target.position) < _attackRange)
+        {
+            Debug.Log("추적 종료");
+
+            return INode.NodeState.Success;
+        }
+
+
+        transform.LookAt(new Vector3(_target.position.x, transform.position.y, _target.position.z));
+
+        transform.Translate(Vector3.forward * 1f * Time.deltaTime);
+
+        return INode.NodeState.Running;
+    }
+
+    INode.NodeState Action1()
+    {
+        Debug.Log("다른 액션도 실행 중");
+
+        return INode.NodeState.Success;
+    }
 
     private void OnDrawGizmos()
     {
